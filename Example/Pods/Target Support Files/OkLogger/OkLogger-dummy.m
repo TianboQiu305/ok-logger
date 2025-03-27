dd3c412e2094b719cc5ad3939a8ad52ad7dba168
Tianbo <61160958+TianboQiu305@users.noreply.github.com>
2025-03-27 10:41:30 +0800

    init

---
+++ Example/Pods/Target Support Files/OkLogger/OkLogger-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_OkLogger : NSObject
+@end
+@implementation PodsDummy_OkLogger
+@end
---
+++ Example/Pods/Target Support Files/Pods-OkLogger_Example/Pods-OkLogger_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_OkLogger_Example : NSObject
+@end
+@implementation PodsDummy_Pods_OkLogger_Example
+@end
